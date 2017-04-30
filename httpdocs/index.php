7e4c3ea0aae4fe1b60e6a8fd8c68c4c13f239f92
dubhunter <the@dubhunter.com>
2017-04-29 21:56:04 -0700

    move to yahoos yql api

--- httpdocs/index.php
+++ httpdocs/index.php
@@ -6,8 +6,6 @@ use Dubhunter\Talon\Http\Response\Json;
 use GuzzleHttp\Client;
 use Phalcon\Mvc\Micro;
 
-define('IPHONE_USER_AGENT', 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_3_3 like Mac OS X) AppleWebKit/601.1 (KHTML, like Gecko) CriOS/52.0.2743.84 Mobile/13G34 Safari/601.1.46');
-
 $app = new Micro();
 
 $app->notFound(function () {
@@ -15,31 +13,41 @@ $app->notFound(function () {
 });
 
 $app->get('/{symbol}', function ($symbol) {
-	$client = new Client([
-		'headers' => [
-			'User-Agent' => IPHONE_USER_AGENT,
-		],
-	]);
-	$response = $client->get('http://finance.yahoo.com/webservice/v1/symbols/' . $symbol . '/quote?format=json&view=detail');
+	$symbol = strtoupper($symbol);
+	$client = new Client();
+	$yql = 'select Symbol, LastTradePriceOnly, Change, PercentChange, DaysLow, DaysHigh, Volume from yahoo.finance.quotes where symbol = "' . $symbol . '"';
+	$response = $client->get(
+		'https://query.yahooapis.com/v1/public/yql',
+		[
+			'query' => [
+				'q' => $yql,
+				'format' => 'json',
+				'env' => 'store://datatables.org/alltableswithkeys',
+			],
+		]
+	);
 	if ($response->getStatusCode() != 200) {
 		return Json::error();
 	}
 	$data = json_decode($response->getBody(), true);
-	if (!isset($data['list'])) {
+	if (!isset($data['query'])) {
 		return Json::error();
 	}
-	if (count($data['list']['resources']) === 0) {
+	if (!isset($data['query']['results'])) {
+		return Json::error();
+	}
+	if (!isset($data['query']['results']['quote'])) {
 		return Json::notFound();
 	}
-	$fields = $data['list']['resources'][0]['resource']['fields'];
+	$fields = $data['query']['results']['quote'];
 	return Json::ok([
-		'symbol' => $fields['symbol'],
-		'price' => round($fields['price'], 2),
-		'change' => round($fields['change'], 2),
-		'change_percent' => round($fields['chg_percent'], 2),
-		'day_high' => round($fields['day_high'], 2),
-		'day_low' => round($fields['day_low'], 2),
-		'volume' => intval($fields['volume']),
+		'symbol' => $fields['Symbol'],
+		'price' => round(floatval($fields['LastTradePriceOnly']), 2),
+		'change' => round(floatval($fields['Change']), 2),
+		'change_percent' => round(floatval($fields['PercentChange']), 2),
+		'day_high' => round(floatval($fields['DaysHigh']), 2),
+		'day_low' => round(floatval($fields['DaysLow']), 2),
+		'volume' => intval($fields['Volume']),
 	]);
 });
 
