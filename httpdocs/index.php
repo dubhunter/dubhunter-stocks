ceb0084defa43ddfa5803eb8d9567c2a7497e628
dubhunter <the@dubhunter.com>
2018-10-15 11:59:31 -0700

    try some caching

--- httpdocs/index.php
+++ httpdocs/index.php
@@ -4,6 +4,8 @@ require '../vendor/autoload.php';
 
 use Dubhunter\Talon\Http\Response\Json;
 use GuzzleHttp\Client;
+use Phalcon\Cache\Frontend\Data as CacheFrontend;
+use Phalcon\Cache\Backend\Libmemcached as Memcache;
 use Phalcon\Di\FactoryDefault;
 use Phalcon\Mvc\Micro;
 
@@ -14,6 +16,32 @@ $di->set('config', function () {
 	return $config;
 });
 
+/**
+ * Setting up the cache
+ */
+$di->set('cache', function() use ($di) {
+	$memcacheConfig = $di->get('config')->get('memcache');
+	$frontCache = new CacheFrontend(array(
+		'lifetime' => $memcacheConfig->lifetimeModels,
+	));
+	return new Memcache($frontCache, array(
+		'servers' => array(
+			array(
+				'host' => $memcacheConfig->host,
+				'port' => $memcacheConfig->port,
+				'weight' => 1,
+			),
+		),
+		'client' => array(
+			Memcached::OPT_PREFIX_KEY => $memcacheConfig->prefix,
+		),
+	));
+}, true);
+
+function cacheKey($symbol) {
+	return 'STOCKS_' . strtoupper($symbol);
+}
+
 $app = new Micro($di);
 
 $app->notFound(function () {
@@ -21,31 +49,38 @@ $app->notFound(function () {
 });
 
 $app->get('/{symbol}', function ($symbol) use ($app) {
-	$client = new Client();
-	$response = $client->get(
-		'https://www.alphavantage.co/query',
-		[
-			'query' => [
-				'apikey' => $app->getDI()->get('config')->alphavantage->key,
-				'function' => 'GLOBAL_QUOTE',
-				'symbol' => strtoupper($symbol),
-			],
-		]
-	);
-	if ($response->getStatusCode() != 200) {
-		return Json::error();
-	}
-	$data = json_decode($response->getBody(), true);
-	if (!is_array($data)) {
-		return Json::error();
-	}
-	$data = array_values($data);
-	if (!isset($data[0])) {
-		return Json::notFound();
-	}
-	$fields = [];
-	foreach ($data[0] as $k => $v) {
-		$fields[str_replace(' ', '_', preg_replace('/^[0-9]+\. /', '', $k))] = $v;
+	/** @var Memcache $cache */
+	$cache = $app->getDI()->get('cache');
+	$cacheKey = cacheKey($symbol);
+	$fields = $cache->get($cacheKey);
+	if (!$fields) {
+		$client = new Client();
+		$response = $client->get(
+			'https://www.alphavantage.co/query',
+			[
+				'query' => [
+					'apikey' => $app->getDI()->get('config')->alphavantage->key,
+					'function' => 'GLOBAL_QUOTE',
+					'symbol' => strtoupper($symbol),
+				],
+			]
+		);
+		if ($response->getStatusCode() != 200) {
+			return Json::error();
+		}
+		$data = json_decode($response->getBody(), true);
+		if (!is_array($data)) {
+			return Json::error();
+		}
+		$data = array_values($data);
+		if (!isset($data[0])) {
+			return Json::notFound();
+		}
+		$fields = [];
+		foreach ($data[0] as $k => $v) {
+			$fields[str_replace(' ', '_', preg_replace('/^[0-9]+\. /', '', $k))] = $v;
+		}
+		$cache->save($cacheKey, $fields, $app->getDI()->get('config')->memcache->lifetime);
 	}
 	return Json::ok([
 		'symbol' => $fields['symbol'],
